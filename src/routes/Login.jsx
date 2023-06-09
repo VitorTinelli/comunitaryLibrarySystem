72eef9380554233d41d8eacfb2ff3498aba5d414
Vitor Tinelli <vitor.mtinelli@gmail.com>
2023-06-09 15:37:01 -0300

    a

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -9,13 +9,7 @@ export default function Index(){
     const createAccount = () => {
         navigateTo("/cadastrar")
     }
-    const submitLogin = () => {
-        if (email == "admin" && password == "admin"){
-            navigateTo("/index")
-        }else{
-            alert("Errou palhaço!")
-        }
-    }
+    
     return (
         <>
             <header>
@@ -23,13 +17,13 @@ export default function Index(){
             </header>
                 <main>
                 <div>
-                    <form action="" method="post">
+                    <form action="" method="post" onSubmit={handleSubmit}>
                         <img src="https://dewey.tailorbrands.com/production/brand_version_mockup_image/401/8395286401_1fea63a5-e908-45e6-8121-62c66b93ece7.png?cb=1685991735" alt="Logotipo" width="150"/>
                         <h1>Bem-vindo(a) à biblioteca!</h1>
-                        <input type="text" name="email" id="email" placeholder="E-mail" required value={email} onChange={(main) => setEmail(mail.target.value)}/>
+                        <input type="text" name="email" id="email" placeholder="E-mail" required value={email} onChange={(mail) => setEmail(mail.target.value)}/>
                         <input type="password" name="senha" id="senha" placeholder="Senha" required value={password} onChange={(pass) => setPassword(pass.target.value)}/>
                         <small>Esqueceu sua senha?</small>
-                        <input type="submit" value="Entrar" onClick={submitLogin}/>
+                        <input type="submit" value="Entrar"/>
                         <small onClick={createAccount}>Ainda não está na biblioteca? Crie uma conta.</small>
                     </form>
                 </div>
