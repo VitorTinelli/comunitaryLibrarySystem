8bf7f9c5ed168fc496293cb57d1c35b505dd3eb4
Vitor Tinelli <vitor.mtinelli@gmail.com>
2023-06-09 18:00:37 -0300

    a

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import "../css/login.css"
+import axios from "axios"
 
 export default function Index(){
     const [email, setEmail] = useState("")
@@ -9,6 +10,20 @@ export default function Index(){
     const createAccount = () => {
         navigateTo("/cadastrar")
     }
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        try {
+          const response = await axios.post('http://localhost:8000/login/', {
+            email,
+            password
+          });
+
+          if (response.status === 201) {
+            console.log('Login realizado com sucesso!');
+          } } catch (error) {
+          console.error(error);
+        }
+      };
     
     return (
         <>
@@ -17,7 +32,7 @@ export default function Index(){
             </header>
                 <main>
                 <div>
-                    <form action="" method="post" onSubmit={handleSubmit}>
+                    <form action="" method="post" onSubmit={handleLogin}>
                         <img src="https://dewey.tailorbrands.com/production/brand_version_mockup_image/401/8395286401_1fea63a5-e908-45e6-8121-62c66b93ece7.png?cb=1685991735" alt="Logotipo" width="150"/>
                         <h1>Bem-vindo(a) à biblioteca!</h1>
                         <input type="text" name="email" id="email" placeholder="E-mail" required value={email} onChange={(mail) => setEmail(mail.target.value)}/>
