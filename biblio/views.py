8400c2a21ca941a840ed44e151373fb4eb3bb02c
Vitor Tinelli <vitor.mtinelli@gmail.com>
2023-06-12 16:27:06 -0300

    a

--- biblio/views.py
+++ biblio/views.py
@@ -3,9 +3,6 @@ from rest_framework import viewsets
 from biblio.models import User
 from biblio.serealizer import UserSerealizer
 
-
-# Create your views here.
-
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerealizer
