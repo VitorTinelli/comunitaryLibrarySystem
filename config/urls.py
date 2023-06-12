96f5a9fafd19ac6f61a51dc9903956ab247de9c3
Vitor Tinelli <vitor.mtinelli@gmail.com>
2023-06-12 16:46:33 -0300

    a

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from biblio.views import UserViewSet
+from biblio.views import BookViewSet
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register(r'usuarios', UserViewSet)
+router.register(r'livros', BookViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
