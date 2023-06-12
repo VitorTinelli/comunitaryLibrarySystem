3d81bfcdffbc4cee9f826e6432004db2305113ab
rafaeldobrasil <githubrafaelbrasil@uorak.com>
2023-06-12 16:19:37 -0300

    a

--- biblio/views.py
+++ biblio/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from biblio.models import User
+from biblio.models import Books
 from biblio.serealizer import UserSerealizer
+from biblio.serealizer import BookSerealizer
 
 
 # Create your views here.
@@ -9,3 +11,6 @@ from biblio.serealizer import UserSerealizer
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerealizer
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Books.objects.all()
+    serializer_class = BookSerealizer
