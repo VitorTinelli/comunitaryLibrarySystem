e49492dd8f262e84d7c4d52eaa58e45fae940809
Vitor Tinelli <vitor.mtinelli@gmail.com>
2023-06-12 16:29:53 -0300

    a

--- biblio/admin.py
+++ biblio/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 from biblio.models import User
+from biblio.models import Books
 # Register your models here.
 
 class Users(admin.ModelAdmin):
-    list_display = ('id', 'username', 'email')
-    list_display_links = ('id','username', 'email')
-    search_fields = ('username', 'email',)
+    list_display = ('id', 'username', 'email', 'isAdm')
+    list_display_links = ('id','username', 'email', 'isAdm')
+    search_fields = ('username', 'email', 'isAdm')
+
+class Bookss(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'genre', 'year', 'edition', 'pages', 'language', 'wiki', 'pdf', 'count')
+    list_display_links = ('id', 'title', 'author', 'genre', 'year', 'edition', 'pages', 'language', 'wiki', 'pdf', 'count')
+    search_fields = ('title', 'author', 'genre', 'year', 'edition', 'pages', 'language',)
 
 admin.site.register(User, Users)
+admin.site.register(Books, Bookss)
--- biblio/models.py
+++ biblio/models.py
@@ -6,6 +6,22 @@ class User(models.Model):
     username = models.CharField(max_length = 50)
     email = models.EmailField()
     password = models.CharField(max_length = 20)
+    isAdm = models.BooleanField(serialize=False)
 
     def __str__(self):
         return self.username
+
+class Books(models.Model):
+    title = models.CharField(max_length = 100)
+    author = models.CharField(max_length = 100)
+    genre = models.CharField(max_length = 100)
+    year = models.CharField(max_length = 4)
+    edition = models.CharField(max_length = 100)
+    pages = models.IntegerField()
+    language = models.CharField(max_length = 100)
+    wiki = models.TextField()
+    pdf = models.TextField()
+    count = models.IntegerField()
+
+    def __str__(self):
+        return self.title
