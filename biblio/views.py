d529746fb8a31efc10e4860927b8ddc4c2ac9f85
Vitor Tinelli <vitor.mtinelli@gmail.com>
2023-06-18 14:35:40 -0300

    loginDone

--- biblio/views.py
+++ biblio/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from biblio.models import User
 from biblio.models import Books
+from rest_framework import generics, status
+from rest_framework.response import Response
 from biblio.serealizer import UserSerealizer
 from biblio.serealizer import BookSerealizer
-
 
 # Create your views here.
 
@@ -14,3 +15,15 @@ class UserViewSet(viewsets.ModelViewSet):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Books.objects.all()
     serializer_class = BookSerealizer
+class LoginView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerealizer
+
+    def create(self, request, *args, **kwargs):
+        email = request.data['email']
+        password = request.data['password']
+
+        if (self.queryset.filter(email = email, password=password).first() is not None):
+            return Response(status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_401_UNAUTHORIZED, data={'erro':'Usuário ou senha inválidos'})
--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
 from biblio.views import UserViewSet
+from biblio.views import BookViewSet
+from biblio.views import LoginView
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register(r'usuarios', UserViewSet)
+router.register(r'livros', BookViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', LoginView.as_view()),
     path('', include(router.urls)),
+
 ]
